31865c7d8fcb40b11309d52851c0741aa49e9351
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-12-25 13:37:14 +0300

    Initial Commit

---
+++ AutismAppTeam20Test/AutismAppTeam20TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutismAppTeam20TestApp.swift
+//  AutismAppTeam20Test
+//
+//  Created by Sumayah Alqahtani on 24/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AutismAppTeam20TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutismAppTeam20TestTests/AutismAppTeam20TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutismAppTeam20TestTests.swift
+//  AutismAppTeam20TestTests
+//
+//  Created by Sumayah Alqahtani on 24/06/1446 AH.
+//
+
+import Testing
+@testable import AutismAppTeam20Test
+
+struct AutismAppTeam20TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
